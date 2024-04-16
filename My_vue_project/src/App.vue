cff6e8056d76d8cbf937e6b42ff7e8e57253709c
Munar <m.suinalievich@gmail.com>
2024-04-16 19:51:32 +0300

    init

---
+++ My_vue_project/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
+
+import{reactive, computed } from 'vue'
+
+const comment = reactive({
+  author: '',
+  stars: null,
+  text: '',
+  photos: [],
+  isRecommended: true
+
+})
+</script>
+
+<template>
+  <div class="container pt-5 pt - 5">
+    <input 
+      type="text"
+      v-model="comment.author"
+      placeholder="Как вас зовут?"
+      class="form-control mb-3"
+    >
+    <textarea 
+      rows="3"
+      v-model="comment.text"
+      class="form-control"
+      placeholder="Сообщение..."
+
+
+      
+    ></textarea>
+  </div>
+</template>
+
+<style scoped>
+</style>
