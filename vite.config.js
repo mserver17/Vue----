6e23a54381dce48ba3b1b31a54f26641c85a7062
Munar <m.suinalievich@gmail.com>
2024-05-09 21:09:25 +0300

    upd

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [
     vue(),
   ],
+  build: {
+    outDir: 'dist',
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     }
   }
-})
+});
